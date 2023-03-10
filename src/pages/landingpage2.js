d20d4f0230d3d52bc2a70b3e3543424940ce0ea2
0xvibes <vibesbeatmaker@gmail.com>
2023-03-10 01:58:35 -0800

    lp2

---
+++ src/pages/landingpage2.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import Art from '../images/ocncrstls.png'
+
+const landingpage2 = () => {
+  return (
+    <div className='lp-main-div'>
+        <div className='art-div'>
+        <Image 
+        src={Art}
+        width={700}
+        height={700}
+        />
+        </div>
+
+        <div className='lp-btns-div'>
+            <Link
+            href="aproject"
+            >
+            <button className='lp-btn'>
+                <b>
+                Go
+                </b>
+            </button >
+            </Link>
+
+            <Link
+            href="aproject"
+            >
+            <button className='lp-btn'>
+                Stay
+            </button>
+            </Link>
+        </div>
+    </div>
+  )
+}
+
+export default landingpage2
