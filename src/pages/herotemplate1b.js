708d382685d73d12fa8dd2f40a63d4c231f4c2e3
0xvibes <vibesbeatmaker@gmail.com>
2023-03-15 15:22:00 -0700

    bg_

--- src/pages/herotemplate1b.js
+++ src/pages/herotemplate1b.js
@@ -5,28 +5,35 @@ import Album from '../images/greenscreen.png'
 
 function herotemplate1b() {
   return (
-    <div className='desktop-background'>
-    <div className='desktop-nav'>
-        <h1 className='desktop-logo'>The Website</h1>
-        <div className='desktop-nav-div'>
-        <h2 className='desktop-nav-links'>Home</h2>
-        <h2 className='desktop-nav-links'>About</h2>
-        <h2 className='desktop-nav-links'>Go Aweay</h2>
+    <>
+    <div className='desktop-background-bg'>
+      <div className='desktop-background'>
+        <div className='desktop-nav'>
+          <h1 className='desktop-logo'>The Website</h1>
+          <div className='desktop-nav-div'>
+          <h2 className='desktop-nav-links'>Home</h2>
+          <h2 className='desktop-nav-links'>About</h2>
+          <h2 className='desktop-nav-links'>Go Aweay</h2>
         </div>
-    </div>
-    <div className='desktop-hero-layout'>
+      </div>
+      <div className='desktop-hero-layout'>
         
         <div className='desktop-hero-image'><Image 
-className='album-image-b'
-src={Album}
-/></div>
-<div className='desktop-hero-text'>
+            className='album-image-b'
+            src={Album}
+            /></div>
+        <div className='desktop-hero-text'>
             <h1 className='desktop-hero-h1'>The Best Freakin Thing Ever</h1>
             <p className='desktop-hero-p'>this part explains more shit.</p>
             <button className='desktop-hero-button'>Do It Now</button>
         </div>
+        
     </div>
+<div className='new-section'>Hello</div>
+
 </div>
+</div>
+</>
   )
 }
 
